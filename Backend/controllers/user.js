90a647e8d1a971ac15f9d166e3c6ccd34b4d4647
sujal <sujalpatil3867@gmail.com>
2026-03-21 16:30:33 +0530

    updated the sql and login/register logic with correct async/await and little form validation

--- Backend/controllers/user.js
+++ Backend/controllers/user.js
@@ -1,74 +1,58 @@
 import bcrypt from "bcrypt";
-import { db } from "../db/sql.js";
+import { pool } from "../db/sql.js";
 import { createToken } from "../utils/jwt.js";
 
 async function register(req, res) {
-    if (!req.body) {
-        return res.status(400).json({ error: "body not present" })
+    if (!req.body.firstName && !req.body.lastName && !req.body.email && !req.body.password) {
+        return res.status(400).json({ success: false, message: "body not present" })
     }
-    const checkUser = `SELECT email from users where email=?`;
-    db.query(checkUser, req.body.email, (error, result) => {
-        if (error) {
-            console.log(error)
-            return res.status(500).json({ error: "server side error" })
-        }
-        if (result.length === 0) {
-            bcrypt.hash(req.body.password, 12).then((hashedPassword) => {
-                const values = [req.body.firstName, req.body.lastName, req.body.email, hashedPassword]
-                const createUser = `INSERT INTO users (firstName,lastName,email,password) VALUES (?,?,?,?)`
-                db.query(createUser, values, ((error, result) => {
-                    if (error) {
-                        console.log(error)
-                    }
-                    if (result) {
-                        return res.status(201).json({ msg: "User created successfully" })
-                    }
-                }))
-            })
-        } else {
-            return res.status(409).json({
-                error: "email id already exist"
-            })
-        }
-    })
+
+    try {
+        // hashing the password
+        const password = await bcrypt.hash(req.body.password, 12);
+        const values = [req.body.firstName, req.body.lastName, req.body.email, password]
+        const createUser = `INSERT INTO users (firstName,lastName,email,password) VALUES (?,?,?,?)`
+        const result = await pool.query(createUser, values);
+        return res.status(201).json({ success: true, message: "user created successfully" })
+    } catch (error) {
+        // if get error that means user already exist
+        return res.status(409).json({ success: false, message: "user already exist" })
+    }
 
 }
 
 async function login(req, res) {
     if (!req.body) {
-        return res.status(400).json({
-            error: "body not present"
-        })
+        return res.status(400).json({ success: false, message: "body not present" })
     }
-    const checkuser = `SELECT * FROM users WHERE email=?;`;
-    db.query(checkuser, req.body.email, (error, result) => {
-        if (error) {
-            console.log(error);
-            return res.status(500).json({
-                error: "Internal server error"
-            })
+
+    try {
+        const query = `select * from users where email=?`;
+        // query to check if the email exist
+        const [rows] = await pool.query(query, req.body.email)
+
+        // if no user then return the responce 
+        if (rows.length === 0) return res.status(401).json({ success: false, message: "email or password is incorrect" })
+
+        // matching the password with the hashvalue of the password
+        const isMatch = await bcrypt.compare(req.body.password, rows[0].password)
+
+        // if password doen not match return responce 
+        if (!isMatch) {
+            return res.status(401).json({ success: false, message: "email or password is incorrect" })
         }
-        if (result.length === 0) {
-            return res.status(400).json({
-                error: "email or password is wrong"
-            })
-        }
-        bcrypt.compare(req.body.password, result[0].password).then((isMatch) => {
-            if (isMatch) {
-                const token = createToken(result[0]);
-                res.cookie("token",token);
-                return res.status(200).json({
-                    msg: "success"
-                })
-            } else {
-                return res.status(400).json({
-                    error: "email or password is wrong"
-                })
-            }
+
+        // create token for the user for further requests
+        const token = createToken(rows[0])
+        res.cookie("token", token, {
+            httpOnly: true,
+            maxAge: 24 * 60 * 60 * 1000,
+            sameSite: "strict"
         })
+        return res.status(200).json({ success: true, message: "login successful" })
+    } catch (error) {
 
-    })
-
+    }
 }
 
 export {
---
+++ Backend/readme.md
@@ -0,0 +1,19 @@
+folder structure 
+
+.
+├── controllers
+│   ├── application.js
+│   └── user.js
+├── db
+│   └── sql.js
+├── index.js
+├── package.json
+├── package-lock.json
+├── routes
+│   ├── application.js
+│   └── user.js
+├── temp.js
+├── utils
+│   └── jwt.js
+└── work.md
+

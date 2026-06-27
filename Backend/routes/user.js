f5640214d8ddddc22b8494563cac36210d446b43
sujal <sujalpatil3867@gmail.com>
2026-06-27 23:11:09 +0530

    added schema to delete user

--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { register, login, logout, deleteUser } from "../controllers/user.js"
-import { loginSchema, registerSchema, deleteUser } from "../schemas/userSchema.js"
+import { loginSchema, registerSchema, deleteAccountSchema } from "../schemas/userSchema.js"
 import validate from "../middleware/validation.js"
 import { verifyUser } from "../middleware/verify.js"
 import { authLimiter } from "../middleware/rateLimiter.js"
@@ -10,7 +10,7 @@ const userRoute = express.Router();
 userRoute.post("/register", authLimiter, validate(registerSchema), register)
 userRoute.post("/login", authLimiter, validate(loginSchema), login)
 userRoute.post("/logout", logout)
-userRoute.delete("/me", verifyUser, validate(deleteUser), deleteUser)
+userRoute.delete("/me", verifyUser, validate(deleteAccountSchema), deleteUser)
 userRoute.get("/me", verifyUser, (req, res) => {
 	return res.status(200).json({ success: true, message: "user verified" })
 })
--- Backend/schemas/deleteUserSchema.js
+++
@@ -1,9 +0,0 @@
-import {z} from "zod"
-
-const deleteAccountSchema=z.object({
-    password:z.string().trim().min(8,"password is required")
-}).strict()
-
-export {
-    deleteAccountSchema
-}

a0ddb30219bffb0af848e6ca6b0959c90105ad84
sujal <sujalpatil3867@gmail.com>
2026-07-08 01:23:00 +0530

    added route to update status of application

--- Backend/routes/application.js
+++ Backend/routes/application.js
@@ -1,25 +1,29 @@
 import express from "express";
 const applicationRoute = express.Router();
 import validate from "../middleware/validation.js"
-import { createApplicationSchema, updateApplicationSchema } from "../schemas/applicationSchema.js";
+import { createApplicationSchema, updateApplicationSchema, updateApplicationStatusSchema } from "../schemas/applicationSchema.js";
 
 import {
     getAppliactions,
     getApplication,
     createApplication,
     deleteApplication,
-    updateApplication
+    updateApplication,
+    updateApplicationStatus
 } from "../controllers/application.js"
 
 applicationRoute
     .route("/")
     .get(getAppliactions)
     .post(validate(createApplicationSchema), createApplication)
-    
+
 applicationRoute
     .route("/:id")
     .get(getApplication)
     .put(validate(updateApplicationSchema), updateApplication)
     .delete(deleteApplication)
+
+applicationRoute
+    .patch('/:id/status', validate(updateApplicationStatusSchema), updateApplicationStatus)
 
 export default applicationRoute;
